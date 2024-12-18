a17801a5724bb69ea022db37da6c0987c1306f7b
Udham Singh <92254626+udham8306@users.noreply.github.com>
2024-12-18 12:36:04 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,11 @@ async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000, // Increase the server selection timeout
-  socketTimeoutMS: 45000, // Increase socket timeout
-  poolSize: 10, // Set a higher pool size
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase the server selection timeout
+     socketTimeoutMS: 45000, // Increase socket timeout
+      poolSize: 10, // Set a higher pool size
+    });
   } catch (error) {
     // console.log("Failed to connect to MongoDB:", error.message);
   }
