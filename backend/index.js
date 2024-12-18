a213ab4892dc6d22980728c0de83f135381c2faa
Udham Singh <92254626+udham8306@users.noreply.github.com>
2024-12-18 15:08:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,6 @@ app.get("/", (req, res) => {
 app.use("/api/user", router); // User routes
 app.use("/api", sellerRouter); // Seller routes
 
-// Error handling middleware for unhandled routes
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Route not found" });
-});
 
 // Error handling middleware for server errors
 app.use((err, req, res, next) => {
